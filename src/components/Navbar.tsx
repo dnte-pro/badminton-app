d83e4c729973856b991f71ac37009d85b44211c5
dnte-pro <kipronoyegon50@hmail.com>
2025-08-29 03:37:37 +0300

    Updating navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-
+import "../css/Navbar.css";
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-white p-4 shadow-md">
+    <nav className="navbar">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">🏸 BadmintonApp</h1>
-        <div className="space-x-6">
-          <Link to="/" className="hover:text-gray-200">Home</Link>
-          <Link to="/courts" className="hover:text-gray-200">Courts</Link>
-          <Link to="/equipment" className="hover:text-gray-200">Equipment</Link>
-          <Link to="/booking" className="hover:text-gray-200">Booking</Link>
-          <Link to="/profile" className="hover:text-gray-200">Profile</Link>
+        <h1> BadmintonApp</h1>
+        <div className="navbar-titles">
+          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/courts" className="navbar-link">Courts</Link>
+          <Link to="/equipment" className="navbar-link">Equipment</Link>
+          <Link to="/booking" className="navbar-link">Booking</Link>
+          <Link to="/profile" className="navbar-link">Profile</Link>
         </div>
       </div>
     </nav>
