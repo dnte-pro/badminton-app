3900990656e4783424398f0ffbe3b2e275f17263
dnte-pro <kipronoyegon50@hmail.com>
2025-09-02 09:27:04 +0300

    navbar setting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,23 @@ import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <h1> BadmintonApp</h1>
-        <div className="navbar-titles">
-          <Link to="/" className="navbar-link">Home</Link>
-          <Link to="/courts" className="navbar-link">Courts</Link>
-          <Link to="/equipment" className="navbar-link">Equipment</Link>
-          <Link to="/booking" className="navbar-link">Booking</Link>
-          <Link to="/profile" className="navbar-link">Profile</Link>
-        </div>
-      </div>
-    </nav>
+    <header className="header">
+        <a href="#home" className="logo">
+            Shuttle<span>Hub</span>
+        </a>
+
+        <i className="bx bx-menu" id="menu-icon" ></i>
+        <nav className="navbar">
+            <a href="/home" >Home</a>
+            <a href="/courts" >Courts</a>
+            <a href="/equipment" >Marketplace</a>
+            <a href="#projects" >Booking</a>
+            <a href="#contact" >Profile</a>
+        </nav>
+
+        <button className="gradient-btn">Contact Me</button>
+
+    </header>
   );
 };
 
